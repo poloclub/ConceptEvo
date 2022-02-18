88f28cbcf4db92ef9f9b68180d22310beb5970bf
Haekyu Park <haekyu@ipsec-10-2-194-150.vpn.gatech.edu>
2022-02-18 15:41:46 -0500

    commit

--- scripts/img_emb-inception_v3.sh
+++ scripts/img_emb-inception_v3.sh
@@ -3,14 +3,14 @@
 # To get image embeddings from a specific model,
 # please give values for --model_nickname and --model_path.
 python main.py \
-    --gpu 2 \
+    --gpu 7 \
     --img_emb T \
     --model_name inception_v3_pretrained \
     --dim 30 \
     --lr_emb 0.01 \
-    --num_emb_epochs 30000 \
-    --num_emb_negs 3 \
+    --num_emb_epochs 5000 \
+    --num_emb_negs 2 \
     --lr_img_emb 10 \
-    --thr_img_emb 0.05 \
+    --thr_img_emb 0.001 \
     --max_iter_img_emb 10000 \
     --k 10

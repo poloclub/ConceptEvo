868ab550b5239de25117c6f2ab8b0a78b7a3f014
Haekyu Park <hkpark627@gmail.com>
2022-12-28 14:14:29 -0500

    A sh for training a model

--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -1,3 +1,4 @@
+##########################################################
 # Provide a correct value for each "?" below:
 # 
 # gpu=?
@@ -22,15 +23,19 @@ weight_decay=0.05
 momentum=0.9
 learning_eps=0.05
 num_epochs=300
+##########################################################
 
-# Provide a correct value for each "?" below to train a model
-# from a pre-trained model
+##########################################################
+# To train a model from a pre-trained one,
+# provide a correct path of the pre-trained model:
+# 
 # model_path=?
 
 # For example:
-model_path=../data/model/convnext_4e-3/data/model-34.pth
+model_path=../data/model/convnext_0.004/data/model-34.pth
+##########################################################
 
-
+##########################################################
 # Train a model from scratch
 python main.py \
     --gpu $gpu \
@@ -45,8 +50,10 @@ python main.py \
     --learning_eps $learning_eps \
     --topk $topk \
     --num_epochs $num_epochs
+##########################################################
 
-# Train a model from a pre-trained model
+##########################################################
+# Train a model from a pre-trained one
 python main.py \
     --gpu $gpu \
     --train T \
@@ -61,32 +68,4 @@ python main.py \
     --learning_eps $learning_eps \
     --topk $topk \
     --num_epochs $num_epochs
-
-
-# InceptionV3
-python main.py \
-    --gpu $gpu \
-    --train T \
-    --training_data ../../ILSVRC2012/train \
-    --test_data ../../ILSVRC2012/val-by-class \
-    --batch_size 64 \
-    --lr 0.045 \
-    --weight_decay 0.9 \
-    --learning_eps 1.0 \
-    --num_epochs 100 \
-    --model_name inception_v3 \
-    --model_nickname inception_v3_0.045_0.9_1.0
-
-# ResNet18
-python main.py \
-    --gpu $gpu \
-    --train T \
-    --training_data ../../ILSVRC2012/train \
-    --test_data ../../ILSVRC2012/val-by-class \
-    --batch_size 256 \
-    --lr 0.1 \
-    --weight_decay 0.0001 \
-    --momentum 0.9 \
-    --num_epochs 300 \
-    --model_name resnet18 \
-    --model_nickname resnet18_0.1
+##########################################################

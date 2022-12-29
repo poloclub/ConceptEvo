e97f56ccd129d9555a633f0c5750ea7bd4ddcd28
Haekyu Park <hkpark627@gmail.com>
2022-12-29 16:33:37 -0500

    scripts

--- scripts/proj_embedding.sh
+++ scripts/proj_embedding.sh
@@ -95,3 +95,50 @@ python main.py \
     --max_iter_img_emb $max_iter_img_emb \
     --k $k
 ###############################################################################
+
+
+###############################################################################
+# If you have multiple models, uncomment BLOCK1 below and run it.
+# 
+# If you get an error saying `Syntax error: "(" unexpected`,  
+# run this script with `bash` command: `bash proj_embedding.sh`.
+###############################################################################
+
+###############################################################################
+# # BLOCK1: if you have multiple models
+# 
+# model_names=( 
+#     model_0
+#     model_1
+#     model_2
+# )
+
+# model_nicknames=( 
+#     nickname_0
+#     nickname_1
+#     nickname_2
+# )
+
+# for i in "${!model_names[@]}"
+# do
+#     model_name=${model_names[i]}
+#     model_nickname=${model_nicknames[i]}
+#     echo $model_name, $model_nickname
+
+#     python main.py \
+#         --gpu $gpu \
+#         --proj_neuron_emb T \
+#         --basemodel_nickname $basemodel_nickname \
+#         --model_name $model_name \
+#         --model_nickname $model_nickname \
+#         --topk_s $topk_s \
+#         --dim $dim \
+#         --lr_emb $lr_emb \
+#         --num_emb_epochs $num_emb_epochs \
+#         --num_emb_negs $num_emb_negs \
+#         --lr_img_emb $lr_img_emb \
+#         --thr_img_emb $thr_img_emb \
+#         --max_iter_img_emb $max_iter_img_emb \
+#         --k $k
+# done
+###############################################################################

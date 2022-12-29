aea5aedec21f7923b636fba5f625065f77984e12
Haekyu Park <hkpark627@gmail.com>
2022-12-29 16:25:41 -0500

    scripts

--- scripts/proj_embedding.sh
+++ scripts/proj_embedding.sh
@@ -10,12 +10,13 @@
 # 
 # ../data
 #     └── embedding
-#             └── emb-<model_nickname>-<apdx1>
+#             └── emb-<basemodel_nickname>-<apdx1>
 #                   └── data
 #                   │     └── emb
 #                   │     │    ├── emb.json     
-#                   │     │    └── img_emb-<apdx2>.txt
-#                   │     └── emb2d
+#                   │     │    ├── img_emb-<apdx2>.txt
+#                   │     │    └── proj_emb-<model_nickname>-<apdx2>.json
+#                   │     └── emb2d 
 #                   └── log
 # <apdx1>: 
 # ```
@@ -40,158 +41,57 @@
 # ```
 ###############################################################################
 
+###############################################################################
+# The result will be saved at `emb2d/` in the above file structure.
+###############################################################################
 
+###############################################################################
+# Provide a correct value for each "?" below:
+# 
+# gpu=?
+# basemodel_nickname=?
+# model_name=?
+# model_nickname=?
+# topk_s=?
+# dim=?
+# lr_emb=?
+# num_emb_epochs=?
+# num_emb_negs=?
+# lr_img_emb=?
+# thr_img_emb=?
+# max_iter_img_emb=?
+# k=?
+# 
+# For example:
+gpu=0
+basemodel_nickname=vgg19_pretrained
+model_name=vgg16
+model_nickname=vgg16-0.01-207
+topk_s=20
+dim=30
+lr_emb=0.01
+num_emb_epochs=100
+num_emb_negs=3
+lr_img_emb=1.0
+thr_img_emb=0.01
+max_iter_img_emb=10000
+k=$topk_s
+###############################################################################
 
+###############################################################################
 python main.py \
-    --gpu 1 \
+    --gpu $gpu \
     --proj_neuron_emb T \
-    --model_name vgg16_pretrained \
-    --model_nickname vgg16_pretrained \
-    --img_emb_path '../data/embedding/inception_v3_pretrained/data/img_emb-dim=30-lr_img_emb=10.0-thr_img_emb=0.001-max_iter_img_emb=20000-k=10.txt' \
-    --dim 30 \
-    --k 10 \
-    --emb_store_dirname 'emb-1'
-
-
-python main.py \
-    --gpu 1 \
-    --proj_neuron_emb T \
-    --model_name inception_v3 \
-    --model_nickname inception_v3-512-0.5-0.9-121 \
-    --model_path ../data/model/inception_v3-512-0.5-0.9/data/model-121.pth \
-    --img_emb_path '../data/embedding/inception_v3_pretrained/data/img_emb-dim=30-lr_img_emb=10.0-thr_img_emb=0.001-max_iter_img_emb=20000-k=10.txt' \
-    --dim 30 \
-    --k 10 \
-    --emb_store_dirname 'emb-1'
-
-
-python main.py \
-    --gpu 1 \
-    --proj_neuron_emb T \
-    --model_name inception_v3 \
-    --model_nickname inception_v3-512-0.5-0.9-11 \
-    --model_path ../data/model/inception_v3-512-0.5-0.9/data/model-11.pth \
-    --img_emb_path '../data/embedding/inception_v3_pretrained/data/img_emb-dim=30-lr_img_emb=10.0-thr_img_emb=0.001-max_iter_img_emb=20000-k=10.txt' \
-    --dim 30 \
-    --k 10 \
-    --emb_store_dirname 'emb-1'
-
-
-python main.py \
-    --gpu 1 \
-    --proj_neuron_emb T \
-    --model_name inception_v3 \
-    --model_nickname inception_v3-512-0.5-0.9-3 \
-    --model_path ../data/model/inception_v3-512-0.5-0.9/data/model-3.pth \
-    --img_emb_path '../data/embedding/inception_v3_pretrained/data/img_emb-dim=30-lr_img_emb=10.0-thr_img_emb=0.001-max_iter_img_emb=20000-k=10.txt' \
-    --dim 30 \
-    --k 10 \
-    --emb_store_dirname 'emb-1'
-
-
-python main.py \
-    --gpu 1 \
-    --proj_neuron_emb T \
-    --model_name inception_v3 \
-    --model_nickname inception_v3-512-1.5-0.9-69 \
-    --model_path ../data/model/inception_v3-512-1.5-0.9/data/model-69.pth \
-    --img_emb_path '../data/embedding/inception_v3_pretrained/data/img_emb-dim=30-lr_img_emb=10.0-thr_img_emb=0.001-max_iter_img_emb=20000-k=10.txt' \
-    --dim 30 \
-    --k 10 \
-    --emb_store_dirname 'emb-1'
-
-
-python main.py \
-    --gpu 1 \
-    --proj_neuron_emb T \
-    --model_name inception_v3 \
-    --model_nickname inception_v3-512-1.5-0.9-71 \
-    --model_path ../data/model/inception_v3-512-1.5-0.9/data/model-71.pth \
-    --img_emb_path '../data/embedding/inception_v3_pretrained/data/img_emb-dim=30-lr_img_emb=10.0-thr_img_emb=0.001-max_iter_img_emb=20000-k=10.txt' \
-    --dim 30 \
-    --k 10 \
-    --emb_store_dirname 'emb-1'
-
-
-python main.py \
-    --gpu 1 \
-    --proj_neuron_emb T \
-    --model_name inception_v3 \
-    --model_nickname inception_v3-512-1.5-0.9-4 \
-    --model_path ../data/model/inception_v3-512-1.5-0.9/data/model-4.pth \
-    --img_emb_path '../data/embedding/inception_v3_pretrained/data/img_emb-dim=30-lr_img_emb=10.0-thr_img_emb=0.001-max_iter_img_emb=20000-k=10.txt' \
-    --dim 30 \
-    --k 10 \
-    --emb_store_dirname 'emb-1'
-
-
-python main.py \
-    --gpu 1 \
-    --proj_neuron_emb T \
-    --model_name vgg16 \
-    --model_nickname vgg16-512-0.05-0.9-11 \
-    --model_path ../data/model/vgg16-512-0.05-0.9/data/model-11.pth \
-    --img_emb_path '../data/embedding/inception_v3_pretrained/data/img_emb-dim=30-lr_img_emb=10.0-thr_img_emb=0.001-max_iter_img_emb=20000-k=10.txt' \
-    --dim 30 \
-    --k 10 \
-    --emb_store_dirname 'emb-1'
-
-
-python main.py \
-    --gpu 1 \
-    --proj_neuron_emb T \
-    --model_name vgg16 \
-    --model_nickname vgg16-512-0.05-0.9-3 \
-    --model_path ../data/model/vgg16-512-0.05-0.9/data/model-3.pth \
-    --img_emb_path '../data/embedding/inception_v3_pretrained/data/img_emb-dim=30-lr_img_emb=10.0-thr_img_emb=0.001-max_iter_img_emb=20000-k=10.txt' \
-    --dim 30 \
-    --k 10 \
-    --emb_store_dirname 'emb-1'
-
-
-python main.py \
-    --gpu 1 \
-    --proj_neuron_emb T \
-    --model_name vgg16 \
-    --model_nickname vgg16-512-0.05-0.9-13 \
-    --model_path ../data/model/vgg16-512-0.05-0.9/data/model-13.pth \
-    --img_emb_path '../data/embedding/inception_v3_pretrained/data/img_emb-dim=30-lr_img_emb=10.0-thr_img_emb=0.001-max_iter_img_emb=20000-k=10.txt' \
-    --dim 30 \
-    --k 10 \
-    --emb_store_dirname 'emb-1'
-
-
-python main.py \
-    --gpu 1 \
-    --proj_neuron_emb T \
-    --model_name vgg16 \
-    --model_nickname vgg16-512-0.01-0.9-5 \
-    --model_path ../data/model/vgg16-512-0.01-0.9/data/model-5.pth \
-    --img_emb_path '../data/embedding/inception_v3_pretrained/data/img_emb-dim=30-lr_img_emb=10.0-thr_img_emb=0.001-max_iter_img_emb=20000-k=10.txt' \
-    --dim 30 \
-    --k 10 \
-    --emb_store_dirname 'emb-1'
-
-
-python main.py \
-    --gpu 1 \
-    --proj_neuron_emb T \
-    --model_name vgg16 \
-    --model_nickname vgg16-512-0.01-0.9-21 \
-    --model_path ../data/model/vgg16-512-0.01-0.9/data/model-21.pth \
-    --img_emb_path '../data/embedding/inception_v3_pretrained/data/img_emb-dim=30-lr_img_emb=10.0-thr_img_emb=0.001-max_iter_img_emb=20000-k=10.txt' \
-    --dim 30 \
-    --k 10 \
-    --emb_store_dirname 'emb-1'
-
-
-python main.py \
-    --gpu 1 \
-    --proj_neuron_emb T \
-    --model_name vgg16 \
-    --model_nickname vgg16-512-0.01-0.9-207 \
-    --model_path ../data/model/vgg16-512-0.01-0.9/data/model-207.pth \
-    --img_emb_path '../data/embedding/inception_v3_pretrained/data/img_emb-dim=30-lr_img_emb=10.0-thr_img_emb=0.001-max_iter_img_emb=20000-k=10.txt' \
-    --dim 30 \
-    --k 10 \
-    --emb_store_dirname 'emb-1'
+    --basemodel_nickname $basemodel_nickname \
+    --model_name $model_name \
+    --model_nickname $model_nickname \
+    --topk_s $topk_s \
+    --dim $dim \
+    --lr_emb $lr_emb \
+    --num_emb_epochs $num_emb_epochs \
+    --num_emb_negs $num_emb_negs \
+    --lr_img_emb $lr_img_emb \
+    --thr_img_emb $thr_img_emb \
+    --max_iter_img_emb $max_iter_img_emb \
+    --k $k
+###############################################################################
